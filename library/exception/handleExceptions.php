b75d85435f6637ab3fd57ccb10050e660a931869
late <late-foxmail.com>
2016-10-09 11:00:16 +0800

    [修改]修改全局错误处理机制

--- library/exception/handleExceptions.php
+++ library/exception/handleExceptions.php
@@ -5,6 +5,9 @@
  * 
  * late-xiao@foxmail.com
  * */
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Debug\Exception\FatalErrorException;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 class lib_exception_handleExceptions {
 
     /**
@@ -32,11 +35,57 @@ class lib_exception_handleExceptions {
     
     public function handleException($e)
     {
-    
+        $this->renderExceptionWithWhoops($e);
     }
     
     public function handleShutdown()
     {
-    
+        $error = error_get_last();
+        if ( ! is_null($error) && $this->isFatal($error['type']))
+        {
+            $this->handleException($this->fatalExceptionFromError($error, 0));
+        }
     }
+    
+/**
+	 * Create a new fatal exception instance from an error array.
+	 *
+	 * @param  array  $error
+	 * @param  int|null  $traceOffset
+	 * @return \Symfony\Component\Debug\Exception\FatalErrorException
+	 */
+	protected function fatalExceptionFromError(array $error, $traceOffset = null)
+	{
+		return new FatalErrorException(
+			$error['message'], $error['type'], 0, $error['file'], $error['line'], $traceOffset
+		);
+	}
+    
+	/**
+	 * Determine if the error type is fatal.
+	 *
+	 * @param  int  $type
+	 * @return bool
+	 */
+	protected function isFatal($type)
+	{
+        $a = in_array($type, [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE]);
+        
+        return $a;
+	}
+	
+	/**
+	 * 使用whoops错误处理组件
+	 * */
+	protected function renderExceptionWithWhoops(Exception $e)
+	{
+	    $whoops = new \Whoops\Run;
+	    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+	
+	    return new SymfonyResponse(
+	        $whoops->handleException($e),
+	        $e->getStatusCode(),
+	        $e->getHeaders()
+	        );
+	}
 }
