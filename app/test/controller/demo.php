7df0bc9a74fa40cbc3a3fc15ec047b1a6d415b2c
late <late-foxmail.com>
2016-10-12 19:18:04 +0800

    [test]测试路由

--- app/test/controller/demo.php
+++ app/test/controller/demo.php
@@ -4,7 +4,7 @@
  * 
  * 
  * */
-class test_ctl_demo{
+class test_ctl_demo extends lib_lib_controller{
     
     public function test()
     {
@@ -15,5 +15,11 @@ class test_ctl_demo{
     public function del()
     {
         view::make('test/dd.html', ['item'=>'this is item']);
+    }
+    
+    public function item($it_id)
+    {
+        $url = url::action('test_ctl_demo@item',['itm_id'=>34,'item-id'=>21]);
+        $this->display('test/item.html', ['id'=>$it_id, 'url'=>$url]);
     }
 }
--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -6,3 +6,4 @@
  * */
 route::get('/', [ 'as' => 'topc', 'uses' => 'test_ctl_demo@test']);
 route::get('del.html', [ 'as' => 'er', 'uses' => 'test_ctl_demo@del']);
+route::get('item-{test}.html', [ 'as' => 'er', 'uses' => 'test_ctl_demo@item']);
